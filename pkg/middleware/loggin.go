6cfc97aa0b31fda0b5eda8b42e6682051e08d85e
johannes60-sk <garilsenan@gmail.com>
2025-02-10 16:26:36 +0100

    chore(): 'enhance logging middleware to include request and response headers'

--- pkg/middleware/loggin.go
+++ pkg/middleware/loggin.go
@@ -11,7 +11,9 @@ func LoggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
 		log.Printf("Début requête %s %s", r.Method, r.URL.Path)
+		log.Printf("En-têtes de la requête : %v", r.Header)
 		next.ServeHTTP(w, r)
+		log.Printf("En-têtes de la réponse : %v", w.Header())
 		log.Printf("Fin requête %s %s en %v", r.Method, r.URL.Path, time.Since(start))
 	})
 }
